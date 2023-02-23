86eecc60ea7573c763a313eeee2617c0fe9802a1
Dharmik Puri Goswami <dharmikpuri2004@gmail.com>
2023-02-23 23:13:11 +0530

    Day-3

--- spiteful-toothbrush-9776/src/AllRoutes/Navbar.jsx
+++ spiteful-toothbrush-9776/src/AllRoutes/Navbar.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import {NavLink} from "react-router-dom"
+import { Box,Text,Image,Container,Center, Heading,VStack,Select,Spacer } from '@chakra-ui/react'
 const link=[
     // {path:"/",title:"Home"},
     {path:"/about",title:"About"},
     {path:"/blog",title:"Blog"},
-    {path:"/career",title:"Career"}
+    {path:"/career",title:"Career"},
+    {path:"/helpSupport",title:"HelpSuppory"}
 ]
 const Navbar = () => {
   return (
     <div>
-        {link.map(({path,title})=>(
-            <NavLink key={path} to={path}>{title}</NavLink>
-        ))}
+        <Container maxW={"1240px"}  mt={"100px"} bg={'black'} color={"white"}>
+            <Center justifyContent={"space-around"} display={'flex'} fontSize={"25px"} p={"10px"}>
+                {link.map((item) =>(
+                  <Box> <Spacer/>
+                      <NavLink to={item.path}>{item.title}</NavLink> 
+                  </Box>
+                ))}
+            </Center>
+
+        </Container>
     </div>
   )
 }
--- spiteful-toothbrush-9776/src/Pages/Home.jsx
+++ spiteful-toothbrush-9776/src/Pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box,Text,Image,Container,Center, Heading } from '@chakra-ui/react'
+import { Box,Text,Image,Container,Center, Heading,VStack,Select } from '@chakra-ui/react'
 const Home = () => {
   return (
     <div>
@@ -9,12 +9,23 @@ const Home = () => {
       <Box>
        <Container>
          <Center>
-         <Text as="h3" style={{fontSize:'24px', fontWeight:"400"}} >
+        <VStack>
+        <Text as="h3" style={{fontSize:'24px', fontWeight:"400"}} >
          Oops! Looks like we don’t serve in your city
          </Text>
          <Heading>
-            
+         Select a different Location
          </Heading>
+         <Box>
+         <Select placeholder='Select option' size={"lg"} width={"500px"}>
+      <option value='option1'>Option 1</option>
+     <option value='option2'>Option 2</option>
+      <option value='option3'>Option 3</option>
+       </Select>
+         </Box>
+        </VStack>
+         
+
          </Center>
        </Container>
       </Box>
